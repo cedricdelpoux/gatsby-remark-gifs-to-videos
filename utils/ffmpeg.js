cf3c756368a998616911e55627905e5f69aabb1a
Cédric Delpoux <cedric.delpoux@gmail.com>
2021-06-09 15:06:35 +0200

    feat: add ffprobe-installer

--- utils/ffmpeg.js
+++ utils/ffmpeg.js
@@ -1,8 +1,10 @@
 const util = require("util")
 const ffmpeg = require("fluent-ffmpeg")
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path
+const ffprobePath = require("@ffprobe-installer/ffprobe").path
 
 ffmpeg.setFfmpegPath(ffmpegPath)
+ffmpeg.setFfprobePath(ffprobePath)
 
 const ffprobe = util.promisify(ffmpeg.ffprobe)
 
